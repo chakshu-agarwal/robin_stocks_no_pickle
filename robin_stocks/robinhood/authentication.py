6828cb619bfbd5a45f154e7075bb12cdeb7a69cc
Chakshu Agarwal <achakshu@gmail.com>
2024-04-16 23:33:46 -0700

    Updated authentication.py to save pickle file to s3

--- robin_stocks/robinhood/authentication.py
+++ robin_stocks/robinhood/authentication.py
@@ -6,6 +6,14 @@ import random
 
 from robin_stocks.robinhood.helper import *
 from robin_stocks.robinhood.urls import *
+import boto3
+from botocore.client import Config
+from botocore.exceptions import NoCredentialsError
+
+class InputRequiredException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 def generate_device_token():
     """This function will generate a token used when loggin on.
@@ -52,9 +60,9 @@ def respond_to_challenge(challenge_id, sms_code):
 
 def login(username=None, password=None, expiresIn=86400, scope='internal', by_sms=True, store_session=True, mfa_code=None, pickle_name=""):
     """This function will effectively log the user into robinhood by getting an
-    authentication token and saving it to the session header. By default, it
+    authentication token and saving it to the session header. IF store_session = True, it
     will store the authentication token in a pickle file and load that value
-    on subsequent logins.
+    on subsequent logins
 
     :param username: The username for your robinhood account, usually your email.
         Not required if credentials are already cached and valid.
@@ -80,12 +88,7 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
 
     """
     device_token = generate_device_token()
-    home_dir = os.path.expanduser("~")
-    data_dir = os.path.join(home_dir, ".tokens")
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-    creds_file = "robinhood" + pickle_name + ".pickle"
-    pickle_path = os.path.join(data_dir, creds_file)
+    
     # Challenge type is used if not logging in with two-factor authentication.
     if by_sms:
         challenge_type = "sms"
@@ -107,39 +110,54 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
     if mfa_code:
         payload['mfa_code'] = mfa_code
 
-    # If authentication has been stored in pickle file then load it. Stops login server from being pinged so much.
-    if os.path.isfile(pickle_path):
-        # If store_session has been set to false then delete the pickle file, otherwise try to load it.
-        # Loading pickle file will fail if the acess_token has expired.
-        if store_session:
-            try:
-                with open(pickle_path, 'rb') as f:
-                    pickle_data = pickle.load(f)
-                    access_token = pickle_data['access_token']
-                    token_type = pickle_data['token_type']
-                    refresh_token = pickle_data['refresh_token']
-                    # Set device_token to be the original device token when first logged in.
-                    pickle_device_token = pickle_data['device_token']
-                    payload['device_token'] = pickle_device_token
-                    # Set login status to True in order to try and get account info.
-                    set_login_state(True)
-                    update_session(
-                        'Authorization', '{0} {1}'.format(token_type, access_token))
-                    # Try to load account profile to check that authorization token is still valid.
-                    res = request_get(
-                        positions_url(), 'pagination', {'nonzero': 'true'}, jsonify_data=False)
-                    # Raises exception is response code is not 200.
-                    res.raise_for_status()
-                    return({'access_token': access_token, 'token_type': token_type,
-                            'expires_in': expiresIn, 'scope': scope, 'detail': 'logged in using authentication in {0}'.format(creds_file),
-                            'backup_code': None, 'refresh_token': refresh_token})
-            except:
-                print(
-                    "ERROR: There was an issue loading pickle file. Authentication may be expired - logging in normally.", file=get_output())
-                set_login_state(False)
-                update_session('Authorization', None)
-        else:
-            os.remove(pickle_path)
+    # If session_store set to True, then define pickle_path
+    if store_session:
+      
+        s3_bucket_name = os.environ.get('PICKLE_S3_BUCKET_NAME')
+        s3_file_name = "robinhood" + pickle_name + ".pickle"
+
+        my_config = Config(
+            region_name = 'us-east-2',
+            signature_version = 's3v4'
+        )
+
+        try:
+            s3 = boto3.client(
+                's3',
+                config=my_config
+            )
+        except NoCredentialsError:
+            print("AWS Log In Issue")
+
+        # If authentication has been stored in pickle file then load it. Stops login server from being pinged so much.
+        try:
+            response = s3.get_object(Bucket=s3_bucket_name, Key=s3_file_name)
+            pickle_data = pickle.loads(response['Body'].read())
+            access_token = pickle_data['access_token']
+            token_type = pickle_data['token_type']
+            refresh_token = pickle_data['refresh_token']
+            # Set device_token to be the original device token when first logged in.
+            pickle_device_token = pickle_data['device_token']
+            payload['device_token'] = pickle_device_token
+            # Set login status to True in order to try and get account info.
+            set_login_state(True)
+            update_session(
+                'Authorization', '{0} {1}'.format(token_type, access_token))
+            # Try to load account profile to check that authorization token is still valid.
+            res = request_get(
+                positions_url(), 'pagination', {'nonzero': 'true'}, jsonify_data=False)
+            # Raises exception is response code is not 200.
+            res.raise_for_status()
+            return({'access_token': access_token, 'token_type': token_type,
+                    'expires_in': expiresIn, 'scope': scope, 'detail': 'logged in using authentication in {0}'.format(s3_file_name),
+                    'backup_code': None, 'refresh_token': refresh_token})
+        except:
+            print(
+                "ERROR: There was an issue loading pickle file. Authentication may be expired - logging in normally.", file=get_output())
+            set_login_state(False)
+            update_session('Authorization', None)
+    else:
+        pass
 
     # Try to log in normally.
     if not username:
@@ -153,16 +171,8 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
     data = request_post(url, payload)
     # Handle case where mfa or challenge is required.
     if data:
-        if 'mfa_required' in data:
-            mfa_token = input("Please type in the MFA code: ")
-            payload['mfa_code'] = mfa_token
-            res = request_post(url, payload, jsonify_data=False)
-            while (res.status_code != 200):
-                mfa_token = input(
-                    "That MFA code was not correct. Please type in another MFA code: ")
-                payload['mfa_code'] = mfa_token
-                res = request_post(url, payload, jsonify_data=False)
-            data = res.json()
+        if 'mfa_required' in data and mfa_code is None:
+            raise InputRequiredException("Please type in the MFA code")
         elif 'challenge' in data:
             challenge_id = data['challenge']['id']
             sms_code = input('Enter Robinhood code for validation: ')
@@ -181,11 +191,11 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
             set_login_state(True)
             data['detail'] = "logged in with brand new authentication code."
             if store_session:
-                with open(pickle_path, 'wb') as f:
-                    pickle.dump({'token_type': data['token_type'],
-                                 'access_token': data['access_token'],
-                                 'refresh_token': data['refresh_token'],
-                                 'device_token': payload['device_token']}, f)
+                # Save the authentication data to S3 bucket
+                s3.put_object(Body=pickle.dumps({'token_type': data['token_type'],
+                                                 'access_token': data['access_token'],
+                                                 'refresh_token': data['refresh_token'],
+                                                 'device_token': payload['device_token']}), Bucket=s3_bucket_name, Key=s3_file_name)
         else:
             raise Exception(data['detail'])
     else:
